7010441c5484a0ebc10ba19abeb0caf0cd540a10
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-05-26 16:43:52 +0700

    Add cell class

--- graphics.py
+++ graphics.py
@@ -42,3 +42,50 @@ class Line():
         canvas.create_line(
             self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_colour, width=2
         )
+
+class Cell():
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        
+        if self.has_left_wall:
+            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(left_wall, "black")
+        else:
+            left_wall = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            self.__win.draw_line(left_wall, "white")
+
+        if self.has_right_wall:
+            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(right_wall, "black")
+        else:
+            right_wall = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            self.__win.draw_line(right_wall, "white")
+
+        if self.has_top_wall:
+            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(top_wall, "black")
+        else:
+            top_wall = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            self.__win.draw_line(top_wall, "white")
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(bottom_wall, "black")
+        else:
+            bottom_wall = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            self.__win.draw_line(bottom_wall, "white")
+        
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 def main():
 
     win = Window(800, 600)
 
-    p1 = Point(100, 400)
-    p2 = Point(300, 400)
-    line = Line(p1, p2)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell3 = Cell(win)
+    cell4 = Cell(win)
 
-    win.draw_line(line, "red")
+    cell2.has_bottom_wall = False
+    cell1.has_left_wall = False
+    cell2.has_right_wall = False
+    cell3.has_top_wall = False
+
+    cell1.draw(100, 100, 200, 200)
+    cell2.draw(200, 200, 300, 300)
+    cell3.draw(300, 300, 400, 400)
+    cell4.draw(400, 400, 500, 500)
 
     win.wait_for_close()
 
