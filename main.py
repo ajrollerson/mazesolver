8e6ecdc4289acfbdc9d49d2812723be1352aa93c
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-05-26 15:35:00 +0700

    Initialise project

---
+++ main.py
@@ -0,0 +1,17 @@
+from graphics import Window, Point, Line
+
+def main():
+
+    win = Window(800, 600)
+
+    p1 = Point(100, 400)
+    p2 = Point(300, 400)
+    line = Line(p1, p2)
+
+    win.draw_line(line, "red")
+
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
+    
