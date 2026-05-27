b370f46ee7c80b7880f206a305f4880e7bed3cf0
ajrollerson <alistair.jrollerson@hotmail.co.uk>
2026-05-27 15:51:27 +0700

    Add Maze class

--- graphics.py
+++ graphics.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window:
     def __init__(self, width, height):
@@ -99,3 +100,41 @@ class Cell():
             self.__win.draw_line(to_line, "red")
         else:
             self.__win.draw_line(to_line, "gray")
+
+class Maze():
+    def __init__(
+      self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        for col in range(self.num_cols):
+            column = []
+            for row in range(self.num_rows):
+                column.append(Cell(self.win))
+            self.__cells.append(column)
+
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self.__draw_cell(i, j)
+        
+
+    def __draw_cell(self, i, j):
+        x1 = self.x1 + (i * self.cell_size_x)
+        y1 = self.y1 + (j * self.cell_size_y)
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        self.__cells[i][j].draw(x1, y1, x2, y2)
+        self.__animate()
+        
+
+    def __animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-from graphics import Window, Point, Line, Cell
+from graphics import Window, Point, Line, Cell, Maze
 
 def main():
 
-    win = Window(800, 600)
+    win = Window(1200, 800)
 
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell3 = Cell(win)
-    cell4 = Cell(win)
-
-    cell2.has_bottom_wall = False
-    cell1.has_left_wall = False
-    cell2.has_right_wall = False
-    cell3.has_top_wall = False
-
-    cell1.draw(100, 100, 200, 200)
-    cell2.draw(200, 200, 300, 300)
-    cell3.draw(300, 300, 400, 400)
-    cell4.draw(400, 400, 500, 500)
-
-    cell1.draw_move(cell3, False)
-    cell2.draw_move(cell3, False)
-    cell3.draw_move(cell4, False)
-    cell3.draw_move(cell4, True)
+    maze = Maze(100, 100, 20, 30, 25, 25, win)
+    
 
 
     win.wait_for_close()
